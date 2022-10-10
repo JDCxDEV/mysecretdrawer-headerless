65aa34cab13e0e5ff52cdbb508cc7bf7045455d4
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2022-10-10 16:04:59 +0800

    fix origin

--- pages/shop/furniture/components/parallax_banner.vue
+++ pages/shop/furniture/components/parallax_banner.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
 <section class="p-0">
-    <div class="full-banner parallax text-center p-left" v-bind:style="{ 'background-image': `url(${imagepath})` }">
+    <div class="full-banner parallax text-center p-left" style="background-size: cover;
+    background-size: 100% 100% !important;
+    height: 50vh !important;" v-bind:style="{ 'background-image': `url(${imagepath})` }">
          <img :src="imagepath" alt class="bg-img d-none" />
         <div class="container">
             <div class="row">
                 <div class="col">
                     <div class="banner-contain" style="text-shadow: 6px 6px 0px rgba(0,0,0,0.2);">
-                        <h2>{{title}}</h2>
-                        <h3 style="color:whitesmoke">{{subtitle}}</h3>
+                        <!-- <h2>{{title}}</h2>
+                        <h3 style="color:whitesmoke">{{subtitle}}</h3> -->
                     </div>
                 </div>
             </div>
@@ -22,7 +24,7 @@
 export default {
   data() {
     return {
-      imagepath: 'https://wallpaperaccess.com/full/4215564.jpg',
+      imagepath: require('@/assets/images/custom/banner/parallax.jpg'),
       title: '2022',
       subtitle: 'My Secret Drawer',
     }
