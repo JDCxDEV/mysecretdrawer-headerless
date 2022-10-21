232592bcfb91ce1e331aebfdf4598244ade2d811
jeremy delacruz <jeremy.delacruz.dev@gmail.com>
2022-10-21 14:48:54 +0800

    fix origin

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -227,7 +227,7 @@
                             <iframe
                               width="560"
                               height="315"
-                              src="https://www.youtube.com/embed/BUWzX78Ye_8"
+                              src="https://now4.cdn.spotlightr.com/watch/MjUwOTY3"
                               allow="autoplay; encrypted-media"
                               allowfullscreen
                             ></iframe>
